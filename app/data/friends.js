593e8f02e80fe0b4ed623cd12871f55d2a209338
Kyle Harrer <harrer.kyle@gmail.com>
2018-05-15 18:00:01 -0400

    html whatnot

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+const friendsArray = [
+	{
+		name: 'Skip',
+		photo: 'https://www.nationalgeographic.com/content/dam/animals/pictures/mammals/d/domestic-dog/domestic-dog.adapt.1900.1.jpg',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Slim',
+		photo: 'https://www.cesarsway.com/dog-behavior/innocuous-behaviors/common-dog-behaviors-explained',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Spot',
+		photo: 'https://www.catersnews.com/wp-content/uploads/2016/02/2_CATERS_PANDOGS_03-800x498.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Barky',
+		photo: 'http://www.slate.com/content/dam/slate/articles/health_and_science/Science/2017/08/170803_MEDEX_SickDog.jpg.CROP.promo-xlarge2.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 // npm packages
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // Configure the Express application
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, "./app/public")));
 
-// middleware for parsing incoming request
+// middleware
+app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
 
 // routes
 require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
 require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 app.listen(PORT, function() {
-  console.log("Friend Finder app is listening on PORT: " + PORT);
+  console.log("Dog Friend Finder is listening on PORT: " + PORT);
 });
